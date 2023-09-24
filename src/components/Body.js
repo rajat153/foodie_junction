bad34e1e2d7c4ecc4f5fdf144697d9f4cf28f387
Rajat Chaurasia <er.rajat153@gmail.com>
2023-09-24 08:16:14 +0530

    new branch

--- src/components/Body.js
+++ src/components/Body.js
@@ -19,6 +19,7 @@ const Body = () => {
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.4358011&lng=81.846311&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const resp = await data.json();
+    console.log(resp)
     let filteredData = resp.data.cards.filter(
       (item) => item.card.card.id == "restaurant_grid_listing"
     );
